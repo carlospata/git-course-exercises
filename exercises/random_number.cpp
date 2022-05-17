1f0d96fe7f35f61d58128ed6c909758c4fbeab70
Carlo Spata <SPTCRL02E26H163H@studium.unict.it>
2022-05-17 17:33:31 +0200

    feat: implement generate a random number

--- exercises/random_number.cpp
+++ exercises/random_number.cpp
@@ -4,3 +4,13 @@
   Output:
   The random number is: 4
 */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main(){
+  srand(time(0));
+
+  std::cout << "The random number is: " << rand()s;
+}
